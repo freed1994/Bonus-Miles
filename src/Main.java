2595fa002150ab1572479d1d9c150d6037b03539
freed1994 <freed94@mail.ru>
2021-05-24 19:45:03 +0300

    java added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000; //rub
+        int priceOneMile = 20; //rub
+        int bonusMiles = ticketPrice / priceOneMile;
+        System.out.println("Bonus Miles:"+ bonusMiles);
+    }
+}
